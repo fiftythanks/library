ce0b6f557a1150296d96f673cdf6d62dff230afe
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-01-30 00:00:45 +0700

    Complete basic addition form
    Styling and polishing remains to be done.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const myLibrary = [];
 
 function Book(
-              title = "",
-              author = "",
-              published = 0,
-              era = "",
-              language = "",
-              genre = "",
-              status = "",
-              rating = 0
-            ) {
+  title = "",
+  author = "",
+  published = 0,
+  era = "",
+  language = "",
+  genre = "",
+  status = "",
+  rating = 0
+) {
   this.title = title;
   this.author = author;
   this.published = published;
@@ -43,75 +43,164 @@ function addBookToLibrary(
   myLibrary.push(newBook);
 }
 
+function createRow(i) { 
+  if (i === undefined) i = myLibrary.length - 1;
+  const table = document.querySelector("tbody");
+  const row = document.createElement("tr");
+
+  const number = document.createElement("td");
+  number.classList.add("order");
+  number.position = i;
+  number.textContent = `${i + 1}`;
+  row.appendChild(number);
+
+  const title = document.createElement("td");
+  title.classList.add("title");
+  title.textContent = myLibrary[i].title;
+  row.appendChild(title);
+
+  const author = document.createElement("td");
+  author.classList.add("author");
+  author.textContent = myLibrary[i].author;
+  row.appendChild(author);
+
+  const published = document.createElement("td");
+  published.classList.add("published");
+  if (myLibrary[i].published) {
+    published.textContent = `${myLibrary[i].published} ${myLibrary[i].era}`;  
+  }
+  row.appendChild(published);
+
+  const language = document.createElement("td");
+  language.classList.add("language");
+  language.textContent = myLibrary[i].language;
+  row.appendChild(language);
+
+  const genre = document.createElement("td");
+  genre.classList.add("genre");
+  genre.textContent = myLibrary[i].genre;
+  row.appendChild(genre);
+
+  const status = document.createElement("td");
+  status.classList.add("status");
+  status.textContent = myLibrary[i].status;
+  row.appendChild(status);
+
+  const rating = document.createElement("td");
+  rating.classList.add("rating");
+  if (myLibrary[i].rating !== 0) {
+    rating.textContent = `${myLibrary[i].rating}/10`;   
+  }
+
+  const removeBtn = document.createElement("button");
+  removeBtn.classList.add("remove-btn");
+  removeBtn.textContent = "Remove";
+  removeBtn.addEventListener("click", () => {
+    myLibrary.splice(number.position - 1, 1);
+    let position = number.position;
+    row.remove();
+    for (let i = position; i < myLibrary.length; i++) {
+      table.children[i].querySelector(".order").textContent = `${i + 1}`;
+      table.children[i].querySelector(".order").position--;
+    }
+  });
+  rating.appendChild(removeBtn);
+
+  row.appendChild(rating);
+
+  table.appendChild(row);
+}
+
 function fillTable() {
   for (let i = 0; i < myLibrary.length; i++) {
-    const table = document.querySelector("tbody");
-    const row = document.createElement("tr");
-
-    const number = document.createElement("td");
-    number.classList.add("order");
-    number.position = i;
-    number.textContent = `${i + 1}`;
-    row.appendChild(number);
-
-    const title = document.createElement("td");
-    title.classList.add("title");
-    title.textContent = myLibrary[i].title;
-    row.appendChild(title);
-
-    const author = document.createElement("td");
-    author.classList.add("author");
-    author.textContent = myLibrary[i].author;
-    row.appendChild(author);
-
-    const published = document.createElement("td");
-    published.classList.add("published");
-    if (myLibrary[i].published !== 0) {
-      published.textContent = `${myLibrary[i].published} ${myLibrary[i].era}`;  
-    }
-    row.appendChild(published);
-
-    const language = document.createElement("td");
-    language.classList.add("language");
-    language.textContent = myLibrary[i].language;
-    row.appendChild(language);
-
-    const genre = document.createElement("td");
-    genre.classList.add("genre");
-    genre.textContent = myLibrary[i].genre;
-    row.appendChild(genre);
-
-    const status = document.createElement("td");
-    status.classList.add("status");
-    status.textContent = myLibrary[i].status;
-    row.appendChild(status);
-
-    const rating = document.createElement("td");
-    rating.classList.add("rating");
-    if (myLibrary[i].rating !== 0) {
-      rating.textContent = `${myLibrary[i].rating}/10`;   
-    }
-
-    const removeBtn = document.createElement("button");
-    removeBtn.classList.add("remove-btn");
-    removeBtn.textContent = "remove";
-    removeBtn.addEventListener("click", () => {
-      myLibrary.splice(number.position - 1, 1);
-      let position = number.position;
-      row.remove();
-      for (let i = position; i < myLibrary.length; i++) {
-        table.children[i].querySelector(".order").textContent = `${i + 1}`;
-        table.children[i].querySelector(".order").position--;
-      }
-    });
-    rating.appendChild(removeBtn);
-
-    row.appendChild(rating);
-
-    table.appendChild(row);
+    createRow(i);
   }
 }
 
-function addBookWithBtn() {
-  
+function createRowWithBtn(
+  title,
+  author,
+  published,
+  era,
+  language,
+  genre,
+  status,
+  rating
+) {
+  addBookToLibrary(
+    title,
+    author,
+    published,
+    era,
+    language,
+    genre,
+    status,
+    rating
+  );
+  createRow();
 }
+
+
+let newBookForm = document.querySelector("form.add");
+newBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let title = e.target.querySelector("#title").value;
+  let author = e.target.querySelector("#author").value;
+  let published = e.target.querySelector("#published").value;
+  let era = e.target.querySelector("#era").value;
+  let language = e.target.querySelector("#language").value;
+  let genre = e.target.querySelector("#genre").value;
+  let status = e.target.querySelector("#status").value;
+  let rating = parseInt(e.target.querySelector("#rating").value);
+
+  createRowWithBtn(
+    title,
+    author,
+    published,
+    era,
+    language,
+    genre,
+    status,
+    rating
+  );
+
+  const dataListTitle = e.target.querySelector("#for-title");
+  let dataListTitleOptions = Array.from(dataListTitle.children).map((child) => {
+    return child.value;
+  });
+  if (title && !(dataListTitleOptions.includes(title))) {
+    const option = document.createElement("option");
+    option.value = title; 
+    dataListTitle.appendChild(option);
+  } 
+  
+  const dataListAuthor = e.target.querySelector("#for-author");
+  let dataListAuthorOptions = Array.from(dataListAuthor.children).map((child) => {
+    return child.value;
+  });
+  if (author && !(dataListAuthorOptions.includes(author))) {
+    const option = document.createElement("option");
+    option.value = author; 
+    dataListAuthor.appendChild(option);
+  } 
+
+  const dataListPublished = e.target.querySelector("#for-published");
+  let dataListPublishedOptions = Array.from(dataListPublished.children).map((child) => {
+    return child.value;
+  });
+  if (published && !(dataListPublishedOptions.includes(published))) {
+    const option = document.createElement("option");
+    option.value = published; 
+    dataListPublished.appendChild(option);
+  } 
+
+  const dataListLanguage = e.target.querySelector("#for-language");
+  let dataListLanguageOptions = Array.from(dataListLanguage.children).map((child) => {
+    return child.value;
+  });
+  if (language && !(dataListLanguageOptions.includes(language))) {
+    const option = document.createElement("option");
+    option.value = language; 
+    dataListLanguage.appendChild(option);
+  }
+});
